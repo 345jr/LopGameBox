e98a7d7cf5dc2fd1cfab7f3ee44246e6d799f0e4
Lopop <loppgljr@gmail.com>
2025-08-20 12:02:23 +0800

    8.20清理仓库

--- src/main/misc/delect.mjs
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs/promises'
-fs.unlink('apple.ts',(err)=>{
-    if (err){
-        console.log('发生错误')
-        return
-    }
-    console.log('删除成功')
-})
--- src/main/misc/path.mjs
+++
@@ -1,6 +0,0 @@
-import path from "path";
-const filePath = "C:\Galgame\game-box\kakenuke_R18\kakenuke.exe"
-const formatPath = filePath.replaceAll('"', '').replaceAll('\\', '/');
-console.log(formatPath)
-const dirPath = path.dirname(formatPath);
-console.log(dirPath);

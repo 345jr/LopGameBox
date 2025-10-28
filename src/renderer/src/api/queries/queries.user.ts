48aeccd3698c7181a77962be2a7daf4d8ce4ac46
Lopop <loppgljr@gmail.com>
2025-10-28 10:15:53 +0800

    chore修改文件夹名称

--- src/renderer/src/api/queries/queries.user.ts
+++ src/renderer/src/api/queries/queries.user.ts
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import * as api from '../originAPI/index';
+import * as api from '../fetchAPI/index';
 import { queryKeys } from '../queryKeys';
 
 /**
